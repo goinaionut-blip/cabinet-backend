5255ea8d5cf893b9158576e764601e9277994278
Ionut Goina <ionut.goina.extern@ssi-schaefer.com>
2026-02-04 15:32:33 +0200

    fix last

--- src/main/java/ro/cabinet/backend/StomaBackendApplication.java
+++ src/main/java/ro/cabinet/backend/StomaBackendApplication.java
@@ -2,11 +2,15 @@ package ro.cabinet.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EntityScan(basePackages = {"ro.cabinet.backend", "ro.stoma.efactura"})
+@EnableJpaRepositories(basePackages = {"ro.cabinet.backend", "ro.stoma.efactura"})
 @EnableScheduling
 public class StomaBackendApplication {
   public static void main(String[] args) {
--- src/main/java/ro/stoma/efactura/EfacturaAutoConfiguration.java
+++ src/main/java/ro/stoma/efactura/EfacturaAutoConfiguration.java
@@ -5,14 +5,10 @@ import ro.stoma.efactura.config.EfacturaProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
 @EnableConfigurationProperties(EfacturaProperties.class)
-@EntityScan(basePackages = "ro.stoma.efactura")
-@EnableJpaRepositories(basePackages = "ro.stoma.efactura")
 @ComponentScan(basePackages = "ro.stoma.efactura")
 @ConditionalOnProperty(prefix = "efactura", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class EfacturaAutoConfiguration {
